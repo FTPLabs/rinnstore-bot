b6c6ba01af1e015fa7bd78d379c8f1692a2d12c4
FTPDev <destrooyy@protonmail.com>
2026-06-04 17:23:09 +0300

    add bot/middlewares/db.py

---
+++ bot/middlewares/db.py
@@ -0,0 +1,16 @@
+from typing import Callable, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from ..database import AsyncSessionFactory
+
+
+class DbSessionMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        async with AsyncSessionFactory() as session:
+            data["session"] = session
+            return await handler(event, data)
